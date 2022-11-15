a5acb5342c18829da440c5bc6f74c041aa4f84de
lgolikova <milagoul@gmail.com>
2022-11-15 18:41:52 +0300

    create app

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Card(props) {
+    return (
+        <>
+            <div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+
+
+
+        </>
+    );
+}
+
+export default Card;
